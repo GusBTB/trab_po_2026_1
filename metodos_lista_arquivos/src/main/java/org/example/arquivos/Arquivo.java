243d0c3ded57e3ce03bacd57ae58dc14d9df9712
GusBTB <gustavocs81@gmail.com>
2026-03-04 08:45:22 -0300

    reorganizando dnv

--- metodos_lista_arquivos/src/main/java/org/example/arquivos/Arquivo.java
+++ metodos_lista_arquivos/src/main/java/org/example/arquivos/Arquivo.java
@@ -7,17 +7,17 @@ public class Arquivo {
     private RandomAccessFile arquivo;
     private int comp, mov;
     public Arquivo(String nomearquivo) {}
-3
+
     public void copiaArquivo(RandomAccessFile arquivoOrigem){}
-    public RandomAccessFile getFile() {}
+    public RandomAccessFile getFile() { return null;}
     public void truncate(long pos) {}
-    public boolean eof() {}
+    public boolean eof() {return false;}
     public void seekArq(int pos) {}
     public void filesize() {}
     public void initComp() {}
     public void initMov() {}
-    public int getComp() {}
-    public int getMov() {}
+    public int getComp() {return 0;}
+    public int getMov() {return 0;}
     public void insercaoDireta() {}
     //demais metodos de ordenacao
     public void geraArquivoOrdenado() {}
